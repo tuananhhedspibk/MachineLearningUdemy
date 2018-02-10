d8e2d487f7e798e2bc9974d88d2892daebd80385
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2018-02-10 12:19:07 +0700

    Part 7

--- Resources/Machine Learning A-Z Template Folder/Part 7 - Natural Language Processing/natural_language_processing.py
+++ Resources/Machine Learning A-Z Template Folder/Part 7 - Natural Language Processing/natural_language_processing.py
@@ -14,12 +14,37 @@ import nltk
 nltk.download("stopwords")
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-# sub: param1 - các kí tự được giữ lại
-review = re.sub('[^a-zA-Z]', " ", dataset["Review"][0])
-review = review.lower()
-
-review = review.split()
 ps = PorterStemmer()
-review = [ps.stem(word) for word in review if not word in set(stopwords.words("english"))]
 
-# stemming: get root of word: loving -> love
+corpus =[]
+# sub: param1 - các kí tự được giữ lại
+
+for i in range(1000):
+    review = re.sub('[^a-zA-Z]', " ", dataset["Review"][i])
+    review = review.lower()
+    
+    review = review.split()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words("english"))]
+    
+    # stemming: get root of word: loving -> love
+    review = " ".join(review)
+    corpus.append(review)
+    
+# Creating bag of word model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features=1500)
+X = cv.fit_transform(corpus).toarray()
+
+y = dataset.iloc[:, 1].values
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
